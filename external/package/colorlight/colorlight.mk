1b4ba9420303410853ae085451e65e7a3d7375ed
Kaizen Sparc <kaizenwoof@integralement.nu>
2024-04-02 00:01:16 +0200

    Fix colorlight config

--- external/package/colorlight/colorlight.mk
+++ external/package/colorlight/colorlight.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-COLORLIGHT_VERSION = 0.1.0
-COLORLIGHT_SITE_METHOD = local
-COLORLIGHT_SITE = $(BR2_EXTERNAL_COLORLIGHT_PATH)/package/colorlight/sources
-COLORLIGHT_LICENSE = MIT
-COLORLIGHT_LICENSE_FILES = LICENSE-MIT
+COLORLIGHT_VERSION = d94bb9ad69f59298f3a96793ab70366866ec4607
+COLORLIGHT_SITE_METHOD = git
+COLORLIGHT_SITE = https://github.com/kaizensparc/colorlight-hdmi.git
+COLORLIGHT_SUBDIR = colorlight
 
 define COLORLIGHT_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BRASHAT_PATH)/package/colorlight/S99colorlight \
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_COLORLIGHT_PATH)/package/colorlight/S99colorlight \
 		$(TARGET_DIR)/etc/init.d/S99colorlight
 endef
 
